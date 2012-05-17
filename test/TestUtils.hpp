6c34319d6c4a647c26bfcd88808fe3fc25363587
Toni Cebrián <ancechu@gmail.com>
2012-05-17 18:12:40 +0200

    Some minor changes to boostrap from release

--- test/TestUtils.hpp
+++ test/TestUtils.hpp
@@ -2,12 +2,27 @@
 #define _TESTUTILS_HPP_ 
 
 #include <iostream>
+#include <utility>
+#include <map>
+#include <sstream>
+
 
 template<class S,class T>
-std::ostream & operator<<(std::ostream & o, const std::pair<S,T> & p)
+std::ostream& operator<<(std::ostream & o, const std::pair<S,T> & p)
 {
       return o << "(" << p.first << ", " << p.second << ")";
 }
+
+template<class K, class V> 
+std::ostream& operator<<(std::ostream& o, const std::map<K,V> & m){
+    o << "{ ";
+    for(auto it=m.begin();it!=m.end();it++){ 
+       o << "{" << it->first <<": " << it->second << "} ";
+    }
+    o << " }";
+    return o;
+}
+
 
 #endif 
 
